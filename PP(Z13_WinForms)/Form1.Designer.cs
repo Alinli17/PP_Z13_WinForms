c2ad1b553cd0ee017dd3863dbfa28eb695a3eefd
alina.timonina172372 <alina.timonina172372@gmail.com>
2020-05-27 19:21:15 +0300

    Добавьте файлы проекта.

---
+++ PP(Z13_WinForms)/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace PP_Z13_WinForms_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Zadanie = new System.Windows.Forms.GroupBox();
+            this.cgbcjr = new System.Windows.Forms.Label();
+            this.roster = new System.Windows.Forms.RichTextBox();
+            this.data = new System.Windows.Forms.Label();
+            this.search = new System.Windows.Forms.RichTextBox();
+            this.Result = new System.Windows.Forms.Button();
+            this.Zadanie.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Zadanie
+            // 
+            this.Zadanie.Controls.Add(this.search);
+            this.Zadanie.Controls.Add(this.data);
+            this.Zadanie.Controls.Add(this.roster);
+            this.Zadanie.Controls.Add(this.cgbcjr);
+            this.Zadanie.Location = new System.Drawing.Point(12, 12);
+            this.Zadanie.Name = "Zadanie";
+            this.Zadanie.Size = new System.Drawing.Size(427, 651);
+            this.Zadanie.TabIndex = 0;
+            this.Zadanie.TabStop = false;
+            this.Zadanie.Text = "Задание";
+            // 
+            // cgbcjr
+            // 
+            this.cgbcjr.AutoSize = true;
+            this.cgbcjr.Location = new System.Drawing.Point(6, 27);
+            this.cgbcjr.Name = "cgbcjr";
+            this.cgbcjr.Size = new System.Drawing.Size(59, 17);
+            this.cgbcjr.TabIndex = 0;
+            this.cgbcjr.Text = "Список:";
+            // 
+            // roster
+            // 
+            this.roster.Location = new System.Drawing.Point(9, 47);
+            this.roster.Name = "roster";
+            this.roster.Size = new System.Drawing.Size(412, 434);
+            this.roster.TabIndex = 1;
+            this.roster.Text = "";
+            // 
+            // data
+            // 
+            this.data.AutoSize = true;
+            this.data.Location = new System.Drawing.Point(6, 484);
+            this.data.Name = "data";
+            this.data.Size = new System.Drawing.Size(107, 17);
+            this.data.TabIndex = 2;
+            this.data.Text = "Поиск по дате:";
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(9, 504);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(412, 137);
+            this.search.TabIndex = 3;
+            this.search.Text = "";
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(109, 672);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(208, 61);
+            this.Result.TabIndex = 4;
+            this.Result.Text = "Результат";
+            this.Result.UseVisualStyleBackColor = true;
+            this.Result.Click += new System.EventHandler(this.Result_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 742);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.Zadanie);
+            this.Name = "Form1";
+            this.Text = "Практика 13";
+            this.Zadanie.ResumeLayout(false);
+            this.Zadanie.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Zadanie;
+        private System.Windows.Forms.RichTextBox search;
+        private System.Windows.Forms.Label data;
+        private System.Windows.Forms.RichTextBox roster;
+        private System.Windows.Forms.Label cgbcjr;
+        private System.Windows.Forms.Button Result;
+    }
+}
+
---
+++ PP(Z13_WinForms)/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace PP_Z13_WinForms_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public abstract class Client
+        {
+            public abstract string PrintInfo();
+            public abstract bool IsClientByDate(DateTime date);
+        }
+        public class Investor : Client
+        {
+            public string Surname { get; set; }
+            public DateTime DepositDate { get; set; }
+            public decimal DepositAmount { get; set; }
+            public double DepositInterest { get; set; }
+
+            public Investor(string surname, DateTime depositDate, decimal depositAmount, double depositInteres)
+            {
+                Surname = surname;
+                DepositDate = depositDate;
+                DepositAmount = depositAmount;
+                DepositInterest = depositInteres;
+            }
+
+            public override string PrintInfo()
+            {
+                string s = "";
+                s+="Фамилия вкладчика: " + Surname + "\n";
+                s +="Дата открытия вклада: " + DepositDate.ToShortDateString() + "\n";
+                s +="Размер вклада: " + DepositAmount + "\n";
+                s +="Процент по вкладу: " + DepositInterest + "%\n";
+                return s;
+            }
+
+            public override bool IsClientByDate(DateTime date)
+            {
+                if (DepositDate == date)
+                    return true;
+                return false;
+            }
+        }
+        public class Creditor : Client
+        {
+            public string Surname { get; set; }
+            public DateTime CreditDate { get; set; }
+            public decimal CreditAmount { get; set; }
+            public double CreditInterest { get; set; }
+            public decimal CreditBalance { get; set; }
+
+            public Creditor(string surname, DateTime creditDate, decimal creditAmount, double creditInterest,
+                            decimal creditBalance)
+            {
+                Surname = surname;
+                CreditDate = creditDate;
+                CreditAmount = creditAmount;
+                CreditInterest = creditInterest;
+                CreditBalance = creditBalance;
+            }
+
+            public override string PrintInfo()
+            {
+                string s = "";
+                s += "Фамилия кредитора: " + Surname + "\n";
+                s += "Дата выдачи кредита: " + CreditDate.ToShortDateString() + "\n";
+                s += "Размер кредита: " + CreditAmount + "\n";
+                s += "Процент по кредиту: " + CreditInterest + "%\n";
+                s += "Остаток долга: " + CreditBalance + "\n";
+                return s;
+            }
+
+            public override bool IsClientByDate(DateTime date)
+            {
+                if (CreditDate == date)
+                    return true;
+                return false;
+            }
+        }
+        public class Organization : Client
+        {
+            public string Name { get; set; }
+            public DateTime AccountDate { get; set; }
+            public int AccountNumber { get; set; }
+            public decimal AccountAmount { get; set; }
+
+            public Organization(string name, DateTime accountDate, int accountNumber, decimal accountAmount)
+            {
+                Name = name;
+                AccountDate = accountDate;
+                AccountNumber = accountNumber;
+                AccountAmount = accountAmount;
+            }
+
+            public override string PrintInfo()
+            {
+                string s = "";
+                s += "Название организации: " + Name + "\n";
+                s += "Дата открытия счета: " + AccountDate.ToShortDateString() + "\n";
+                s += "Номер счета: " + AccountNumber + "\n";
+                s += "Сумма на счету: " + AccountAmount + "\n";
+                return s;
+            }
+
+            public override bool IsClientByDate(DateTime date)
+            {
+                if (AccountDate == date)
+                    return true;
+                return false;
+            }
+        }
+        private void Result_Click(object sender, EventArgs e)
+        {
+            string rez = "";
+            List<Client> clientList = new List<Client>();
+            StreamReader sr = new StreamReader(@"C:\Users\Алина\Desktop\file.txt", Encoding.Default);
+            while (!sr.EndOfStream)
+            {
+                string[] s = sr.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                if (s.Length == 5)
+                {
+                    clientList.Add(new Creditor(s[0], Convert.ToDateTime(s[1]), Convert.ToDecimal(s[2]),
+                                                      Convert.ToDouble(s[3]), Convert.ToDecimal(s[4])));
+                }
+                if (s.Length == 4)
+                {
+                    int number;
+                    if (Int32.TryParse(s[2], out number))
+                    {
+                        clientList.Add(new Organization(s[0], Convert.ToDateTime(s[1]),
+                                                     number, Convert.ToDecimal(s[3])));
+                    }
+                    else
+                    {
+                        clientList.Add(new Investor(s[0], Convert.ToDateTime(s[1]),
+                                  Convert.ToDecimal(s[2]), Convert.ToDouble(s[3])));
+                    }
+                }
+            }
+            sr.Close();
+            foreach (Client client in clientList)
+            {
+                rez +=client.PrintInfo()+ "\n";
+            }
+            roster.Text = rez.ToString();
+
+            DateTime askDate = new DateTime(2019, 8, 9);
+            int foundClients = 0;
+
+            foreach (Client client in clientList)
+            {
+                if (client.IsClientByDate(askDate))
+                {
+                    search.Text = client.PrintInfo();
+                    foundClients++;
+                }
+            }
+            if (foundClients == 0)
+            {
+                search.Text = "Клиенты по данной дате не найдены!";
+            }
+        }
+    }
+}
